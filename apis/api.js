0a69554c076d6a771baf1761110122d8e1a4d6c3
Manish Kumar <manishchaudhary2705@gmail.com>
2024-08-13 04:16:56 +0530

    change in the apis routes

--- apis/api.js
+++ apis/api.js
@@ -3,32 +3,32 @@
 export const user = {
     login: {   //done
         method: 'post',
-        url: 'http://localhost:8001/api/v1/users/login',
+        url: '/api/v1/users/login',
     },
 
     createUser: { //done
         method: 'post',
-        url: 'http://localhost:8001/api/v1/users/register',
+        url: '/api/v1/users/register',
     },
 
     getAllRetailers: {  //done
         method: 'get',
-        url: 'http://localhost:8001/api/v1/users/',
+        url: '/api/v1/users/',
     },
 
     updateRetailers: {
         method: 'put',
-        url: 'http://localhost:8001/api/v1/users/:id'
+        url: '/api/v1/users/:id'
     },
 
     deleteRetailer: {
         method: 'delete',
-        url: 'http://localhost:8001/api/v1/users/:id'
+        url: '/api/v1/users/:id'
     },
 
     updateToken: {
         method: 'put',
-        url: 'http://localhost:8001/api/v1/users'
+        url: '/api/v1/users'
     }
 };
 
@@ -36,21 +36,21 @@ export const user = {
 export const application = {
     getAllApplications: {
         method: 'get',
-        url: 'http://localhost:8001/api/v1/applications/',
+        url: '/api/v1/applications/',
     },
 
     updateApplicationStatus: {
         method: 'put',
-        url: 'http://localhost:8001/api/v1/applications',
+        url: '/api/v1/applications',
     },
 
     getRetailersAppliactions: {
         method: 'get',
-        url: 'http://localhost:8001/api/v1/applications/retailer',
+        url: '/api/v1/applications/retailer',
     },
 
     createApplication: {
         method: 'post',
-        url: 'http://localhost:8001/api/v1/applications/',
+        url: '/api/v1/applications/',
     }
 };
