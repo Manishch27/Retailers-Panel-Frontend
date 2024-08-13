eb1fd536b9e754251404ab4bb33a3bcfcee45734
Manish Kumar <manishchaudhary2705@gmail.com>
2024-08-13 06:22:01 +0530

    button text color change on hover

--- src/components/ui/button.jsx
+++ src/components/ui/button.jsx
@@ -9,7 +9,7 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
-        default: "bg-[#ffc107] text-[#00000]-foreground hover:bg-primary/90",
+        default: "bg-[#ffc107] text-[#00000]-foreground hover:bg-primary/90 hover:text-[#fff]",
         destructive:
           "bg-destructive text-destructive-foreground hover:bg-destructive/90",
         outline:
